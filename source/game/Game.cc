5e3024cbcac7a3ad839773651f336d9896612fcb
o2161405 <91959604+o2161405@users.noreply.github.com>
2026-01-05 19:40:45 +0000

    Formatting

--- source/game/Game.cc
+++ source/game/Game.cc
@@ -13,9 +13,9 @@ void Game::run() {
     // while (m_state.ante != WIN_ANTE) {
 
     m_currentAnte = m_anteGenerator.generate(m_state.ante, m_prng);
-    
+
     UIChoice::Action<UIChoice::BlindSelection> action = m_ui.getBlindAction(m_currentAnte);
-    
+
     std::visit(overloaded{[&](const UIChoice::BlindSelection &) {
                               std::println("Blind selection chosen\n");
                           },
